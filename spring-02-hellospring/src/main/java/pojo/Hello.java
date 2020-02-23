45d28e02fcc1d3fad62e63bd67bc6901a4ae873c
Dell 5370 <724033319@qq.com>
2020-02-23 23:19:08 +0800

    holleSpring

---
+++ spring-02-hellospring/src/main/java/pojo/Hello.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class Hello {
+    public String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
